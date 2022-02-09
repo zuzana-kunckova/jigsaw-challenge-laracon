fb5f5c0c7ed2b3dcb5722259f60334f8c6121002
Zuzana K <zk0433@gmail.com>
2022-02-09 22:19:02 +0000

    initial commit

---
+++ source/index.blade.php
@@ -0,0 +1,11 @@
+@extends('_layouts.main')
+
+@section('body')
+<div>
+    <h1 class="text-center">👋 Hello, Laracon! 👋</h1>
+    <div class="w-2/3 mx-auto">
+        <img class="object-contain mx-auto mt-24" src="./assets/img/norberta-laracon.png" alt="">
+        <div class="w-3/5 mx-auto text-center mt-10">Tweet a link to a site you built tagging <a href="https://twitter.com/JigsawByTighten">@JigsawbyTighten</a> and #BuiltWithJigsaw for a chance to win Tighten swag.</div>
+    </div>
+</div>
+@stop
